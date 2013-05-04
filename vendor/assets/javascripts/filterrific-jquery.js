4952e9bb10ff336638b0f77af7c9b8bdb1839c34
Jo Hund <jhund@clearcove.ca>
2013-05-03 19:30:00 -0700

    Tightened up whitespace

--- vendor/assets/javascripts/filterrific-jquery.js
+++ vendor/assets/javascripts/filterrific-jquery.js
@@ -15,51 +15,38 @@
 // Copied from https://github.com/splendeo/jquery.observe_field
 //
 (function($) {
-
   $.fn.observe_field = function(frequency, callback) {
-
     frequency = frequency * 1000; // translate to milliseconds
-
     return this.each(function(){
       var $this = $(this);
       var prev = $this.val();
-
       var check = function() {
         if(removed()){ // if removed clear the interval and don't fire the callback
           if(ti) clearInterval(ti);
           return;
         }
-
         var val = $this.val();
         if(prev != val){
           prev = val;
           $this.map(callback); // invokes the callback on $this
         }
       };
-
       var removed = function() {
         return $this.closest('html').length == 0
       };
-
       var reset = function() {
         if(ti){
           clearInterval(ti);
           ti = setInterval(check, frequency);
         }
       };
-
       check();
       var ti = setInterval(check, frequency); // invoke check periodically
-
       // reset counter after user interaction
       $this.bind('keyup click mousemove', reset); //mousemove is for selects
     });
-
   };
-
 })(jQuery);
-
-
 
 
 
@@ -87,6 +74,7 @@ Filterrific.submitFilterForm = function(){
     $('.filterrific_spinner').hide();
   });
 };
+
 
 
 // Initialize Filterrific event observers
