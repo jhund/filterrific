858c440f5c67c1ec7da001461ffa45f888f4d39f
Jo Hund <jhund@clearcove.ca>
2013-05-16 06:40:28 -0700

    Override ActionView's form_for to add filterrific magic when applied to a Filterrific object:
    * set field name prefix to filterrific
    * override html method
    * set dom id to :filterrific_filter to trigger js observers
    * set url to current URL so that form submits back to self

--- lib/filterrific/action_view_extension.rb
+++ lib/filterrific/action_view_extension.rb
@@ -3,6 +3,18 @@
 #
 module Filterrific::ActionViewExtension
 
+  # Sets all options on form_for to defaults if called with Filterrific object
+  def form_for(record, options = {}, &block)
+    if record.is_a?(Filterrific::ParamSet)
+      options[:as] ||= :filterrific
+      options[:html] ||= {}
+      options[:html][:method] ||= :get
+      options[:html][:id] ||= :filterrific_filter
+      options[:url] ||= url_for
+    end
+    super
+  end
+
   # Renders a spinner while the list is being updated
   def render_filterrific_spinner
     %(
