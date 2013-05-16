1b1eaf25f91153895ff673948140ade5a67d75c4
Jo Hund <jhund@clearcove.ca>
2013-05-16 06:37:46 -0700

    Added simple wrapper for Filterrific::ParamSet.new so that it can be instantiated with Filterrific.new.

--- lib/filterrific.rb
+++ lib/filterrific.rb
@@ -2,4 +2,10 @@ require 'filterrific/version'
 require 'filterrific/engine'
 
 module Filterrific
+
+  # Wrapper around Filterrific::ParamSet initialization
+  def self.new(a_resource_class, filterrific_params = {})
+    Filterrific::ParamSet.new(a_resource_class, filterrific_params)
+  end
+
 end
