7c5c356514901254828f06c96d0e0db3149eaf64
Jo Hund <jhund@clearcove.ca>
2014-10-18 08:54:54 -0700

    Change how we load code: Need to extend ActiveRecord outside of initializer block.
    Otherwise we get exceptions when another library tries to load a class that uses filterrific. `filterrific` won't be known yet and raises an exception.

--- lib/filterrific/engine.rb
+++ lib/filterrific/engine.rb
@@ -1,3 +1,7 @@
+require 'filterrific/param_set'
+require 'filterrific/action_view_extension'
+require 'filterrific/active_record_extension'
+
 module Filterrific
   class Engine < ::Rails::Engine
 
@@ -6,26 +10,16 @@ module Filterrific
 
     isolate_namespace Filterrific
 
-    require 'filterrific/param_set'
+    ActiveSupport.on_load :active_record do
+      extend Filterrific::ActiveRecordExtension
+    end
+
+    ActiveSupport.on_load :action_view do
+      include Filterrific::ActionViewExtension
+    end
 
     initializer "filterrific" do |app|
-
-      ActiveSupport.on_load :active_record do
-        require 'filterrific/active_record_extension'
-        class ::ActiveRecord::Base
-          extend Filterrific::ActiveRecordExtension::ClassMethods
-        end
-      end
-
-      ActiveSupport.on_load :action_view do
-        require 'filterrific/action_view_extension'
-        class ::ActionView::Base
-          include Filterrific::ActionViewExtension
-        end
-      end
-
       app.config.assets.precompile += %w(filterrific-spinner.gif)
-
     end
 
   end
