be9673146ea268b0e33ec920ced976d8bbe2a552
Ben Trewern <ben.trewern@gmail.com>
2019-01-20 19:45:48 +0000

    Remove auto init

--- app/assets/javascripts/filterrific/filterrific-jquery.js
+++ app/assets/javascripts/filterrific/filterrific-jquery.js
@@ -93,16 +93,3 @@ Filterrific.init = function() {
     Filterrific.submitFilterForm
   );
 };
-
-
-// Initialize event observers on document ready and turbolinks page:load
-jQuery(document).on('turbolinks:load', function() {
-  // Prevent double initilisation. With turbolinks 5 this function
-  // will be called twice: on 'ready' and 'turbolinks:load'
-  jQuery(document).off('ready page:load')
-  Filterrific.init();
-});
-
-jQuery(document).on('ready page:load', function() {
-  Filterrific.init();
-});
